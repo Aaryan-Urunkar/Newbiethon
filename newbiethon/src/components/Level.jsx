9924b368f6734bf54bef97bfdbe4f5bd9e8c26ae
Decay_Destructor <aaryanmantri29@gmail.com>
2024-02-23 12:01:43 +0530

    progress

--- newbiethon/src/components/Level.jsx
+++ newbiethon/src/components/Level.jsx
@@ -1,4 +1,11 @@
+import Progress from './Progress'
+import styles from '../css/Level.module.css'
 const Level = () => {
-  return <div>Level</div>
+  return (
+    <div>
+      <h3 className={styles.level}>Level</h3>
+      <progress value={0.5} style={{ backgroundColor: 'white' }} />
+    </div>
+  )
 }
 export default Level
