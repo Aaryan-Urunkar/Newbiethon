42c93c95965ce01d70280f5ffe52ae52db748868
Decay_Destructor <aaryanmantri29@gmail.com>
2024-02-23 12:54:18 +0530

    completed dashboard

--- newbiethon/src/App.jsx
+++ newbiethon/src/App.jsx
@@ -3,14 +3,37 @@ import Navbar from './components/Navbar'
 import './App.css'
 import { Addpage } from './components/Addpage/Addpage'
 import Content from './components/Content'
-function App() {
-  const [count, setCount] = useState(0)
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// function App() {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <>
+//       <Navbar />
+//       <Content />
+//     </>
+//   )
+// }
+const router = createBrowserRouter([
+  {
+    path: '/',
+
+    children: [
+      // { path: "login", element: <Login /> },
+      {
+        path: 'meditracker',
+        children: [
+          { path: 'dashboard', element: <Content /> },
+          { path: 'prescriptionadd', element: <Addpage /> },
+        ],
+      },
+    ],
+  },
+])
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <Content />
-    </>
+    <div>
+      <RouterProvider router={router} />
+    </div>
   )
 }
-
 export default App
